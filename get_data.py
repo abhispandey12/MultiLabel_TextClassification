bee6173864f4208e66332230cabc20f023bb2ab0
poigal <poigal@live.in>
2018-04-12 17:07:37 +0100

    -comments corrected

--- get_data.py
+++ get_data.py
@@ -16,7 +16,7 @@ def cleantxt(txt):
 
 def load_data():
     """
-    Loads data from FTEST if *test* is True, otherwise from FTRAIN.
+    Loads data from data.csv and returns train(20%), val(20%) and test(20%) splits
     Important that the files are in a `data` directory
     """  
     df = pd.read_csv("data/data.csv")
